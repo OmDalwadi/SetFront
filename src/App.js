6f6243e7f22fa8905d2125f55f65c0600267b9b9
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-11 02:09:24 -0600

    Cards page updated
    To make it work make sure that the version are the same and to install the tailwind for the react environment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,75 @@ import './App.css';
 import Login from './components/Login';
 // import LoginForm from './components/loginform';
 import Signup from './components/Signup';
+import Card from './components/Card';
 
+
+// function App() {
+//   return (
+//     <Card />
+//   );
+// }
+
+// export default App;
+
+
+export const plans = [
+  {
+    name: 'Hobby',
+    price: 19,
+    frequency: 'month',
+    features: [
+      'Nullam gravida elementum',
+      'Mauris mauris neque',
+      'Duis auctor tincidunt leo',
+    ],
+  },
+  {
+    name: 'Growth',
+    price: 49,
+    frequency: 'month',
+    featured: true,
+    features: [
+      'Nullam gravida elementum',
+      'Mauris mauris neque',
+      'Duis auctor tincidunt leo',
+    ],
+  },
+  {
+    name: 'Scale',
+    price: 99,
+    frequency: 'month',
+    features: [
+      'Nullam gravida elementum',
+      'Mauris mauris neque',
+      'Duis auctor tincidunt leo',
+    ],
+  },
+];
 
 function App() {
   return (
-    <Signup />
+    <div className="h-full flex flex-col">
+     
+
+     
+       <main className="flex-1 px-6 py-12 lg:flex lg:justify-center lg:items-center">
+        <div className="grid gap-12 lg:gap-0 lg:grid-cols-3">
+          {plans.map(plan => (
+            <div
+              key={plan.name}
+              className={`w-full max-w-lg mx-auto ${
+                plan.featured
+                  ? 'order-first lg:order-none lg:transform lg:scale-110 lg:z-10'
+                  : 'lg:transform lg:scale-90'
+              }`}
+            >
+              <Card {...plan} />
+            </div>
+          ))}
+        </div>
+      </main>
+     </div>
   );
 }
-
 export default App;
