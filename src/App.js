967180b29330c4e99dc0d0082f61071e68867da9
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-11 00:36:07 -0600

    Login-signup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+// import LoginForm from './components/loginform';
+import Signup from './components/Signup';
+
+
+function App() {
+  return (
+    <Signup />
+  );
+}
+
+export default App;
